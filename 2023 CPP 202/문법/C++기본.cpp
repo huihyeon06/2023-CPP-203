22f2695e373f2b8c9080cacb6dce540d4399624e
huihyeon06 <agnea0829@gmail.com>
2023-03-17 09:15:12 +0900

    C++ 스타일1

--- 2023 CPP 202/문법/C++기본.cpp
+++ 2023 CPP 202/문법/C++기본.cpp
@@ -1,6 +1,9 @@
-﻿#include<stdio.h>
+﻿// cout객체를 사용하기 위해 iostreamd을 씀
+#include<iostream>
 
 int main(void) {
-	printf("Hello World\n");
+
+	// << 출력연산자
+	std::cout << "Hello World\n";
 	return 0;
 }
