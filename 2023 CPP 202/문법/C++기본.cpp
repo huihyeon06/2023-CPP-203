99139ed20ea5c8eadf1dd943bc5ec5d5fa8ce746
huihyeon06 <agnea0829@gmail.com>
2023-03-17 09:45:00 +0900

    namespace

--- 2023 CPP 202/문법/C++기본.cpp
+++ 2023 CPP 202/문법/C++기본.cpp
@@ -1,10 +1,13 @@
 ﻿// cout객체를 사용하기 위해 iostreamd을 씀
 #include<iostream>
 
+//이름공간(변수이름의 중복 방지)
+using namespace std;
+
 int main(void) {
 
 	// <<  : 출력연산자
 	//endl : 개행
-	std::cout << "Hello World\n" << std::endl;
+	cout << "Hello World\n" << endl;
 	return 0;
 }
