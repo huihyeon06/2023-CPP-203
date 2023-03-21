b4f6e093be9e826f0ff4ed9e164764bd8f1fc4f6
huihyeon06 <agnea0829@gmail.com>
2023-03-21 14:24:34 +0900

    Reference(참조)

--- 2023 CPP 202/참조 문제.cpp
+++ 2023 CPP 202/참조 문제.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-void sum2(int* num) {
-	*num += 2;
+void sum2(int& num) {
+	num += 2;
 }
 
 int main(void) {
 	int a = 3;
-	sum2(&a);
+	sum2(a);
 	cout << a << endl;
 	return 0;
 }
