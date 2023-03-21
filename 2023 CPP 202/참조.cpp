0c1c61f2648584087a540009771ca7c65f808da2
huihyeon06 <agnea0829@gmail.com>
2023-03-21 13:56:50 +0900

    참조reference

--- 2023 CPP 202/참조.cpp
+++ 2023 CPP 202/참조.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 int main(void) {
 	int a = 30; //a: 30
-	int* pa = &a; //pa: a의 주소값
+	int& ra = a; //pa: a의 주소값
 
-	*pa = 100; //pa가 가리키고 있는 대상(a) : 100
+	//c++은 &안줘도 바꿀 수 있음
+	//*pa = 100 ==> c언어의 경우
+	ra = 100; //pa가 가리키고 있는 대상(a) : 100
 	cout << "a의 값은" << a << endl;
 
 	return 0;
