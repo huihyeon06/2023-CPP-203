7fa081d50a52f0f67cc08921371707bec1e68f49
huihyeon06 <agnea0829@gmail.com>
2023-03-28 13:56:06 +0900

    C스타일 문자열비교 strcmp

--- 2023 CPP 202/문법/문자열 비교.cpp
+++ 2023 CPP 202/문법/문자열 비교.cpp
@@ -11,36 +11,18 @@ int main(void) {
 	char str1[50] = "Hello";
 	char str2[50] = "World";
 
-	const int s1_l = strlen(str1);
-	const int s2_l = strlen(str2);
+	//문자열비교
+	int result = strcmp(str1, str2);
 
-	//s1_l과 s2_l 중 최솟값
-	const int l = s1_l < s2_l ? s1_l : s2_l;
-
-	int i = 0; 
-	while (i < l) {
-		if (str1[i] > str2[i]) {
-			printf("str1>str2\n");
-			break;
-		}
-		else if (str1[i] < str2[i]) {
-			printf("str1<str2\n");
-			break;
-		}
-		i++;
+	if (result == 0) {
+		printf("str1 == str2");
 	}
-	if (i == 1) {
-		if (s1_l > s2_l) {
-			printf("str1>str2\n");
-		}
-		else if (s1_l < s2_l) {
-			printf("str1<str2\n");
-		}
-		else {
-			printf("str1==str2\n");
-		}
+	else if (result == -1) {
+		printf("str1 < str2");
 	}
-	
+	else if (result == 1) {
+		printf("str1 > str2");
+	}
 
 	
 	return 0;
