acfa8679b65a6a4e0fa145f0aa0cde46a48c036c
huihyeon06 <agnea0829@gmail.com>
2023-03-31 08:57:52 +0900

    구조체 기본

---
+++ 2023 CPP 202/문법/c++로 변환.cpp
@@ -0,0 +1,27 @@
+﻿//C++
+#include<iostream>
+#include<string> // std::string 클래스
+//C
+#include<stdio.h>
+#include<string.h>
+
+using namespace std;
+
+int main() {
+    string str1 = "Hello";
+    string str2 = "Hello";
+    
+    if (str1 == str2) {
+        cout << "str1=str2";
+    }
+    else if (str1 < str2) {
+        cout << "str1<str2";
+    }
+    else {
+        cout << "str1>str2";
+    }
+
+    return 0;
+}
+
+
