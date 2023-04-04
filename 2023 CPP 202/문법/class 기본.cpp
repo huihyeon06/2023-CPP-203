df7d3dec1be46c54a38ced964064adc804c58938
huihyeon06 <agnea0829@gmail.com>
2023-04-04 13:51:30 +0900

    생성자 생성

--- 2023 CPP 202/문법/class 기본.cpp
+++ 2023 CPP 202/문법/class 기본.cpp
@@ -11,21 +11,32 @@ class Student {
 	// 성능때문에 studentId를 문자열로 하지 않음
 	// 일반적으로 문자열은 정수형 데이터보다 많은 메모리 공간을 요구하며
 	// 정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자수 만큼 반복하여 비교해야함
+
 public:
+	//생성자 (constructor) : 객체를 생성할 때 호출되는 함수
+	//생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다
+	Student() {
+		home = "김포";
+		department = "소프트웨어과";
+		name = "김씨";
+		studentId = 2211;
+		telephone = "123-4567-8912";
+
+		print();
+	}
+	// class 멤버 함수를 가질 수 있다
+	void print(void) {
+		cout << "이름 : " << name << "\n과 : " << department << "\n학번 : " << studentId <<
+			"\n전화번호 : " << telephone << "\n집주소 : " << home << endl;
+	}
+
+private:
 	int studentId;
 	string name;
 	string telephone;
 	string department;
 	string home;
 
-	//생성자 (constructor) : 객체를 생성할 때 호출되는 함수
-	//생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다
-
-	// class 멤버 함수를 가질 수 있다
-	void print(void) {
-		cout << "이름 : " << name << "\n과 : " <<department << "\n학번 : " << studentId <<
-			"\n전화번호 : " << telephone << "\n집주소 : " << home << endl;
-	}
 };
 
 int main(void) {
@@ -33,15 +44,6 @@ int main(void) {
 	// 자료형 : Student(Class 생략 가능)
 	Student kim;
 
-	kim.home = "김포";
-	kim.department = "소프트웨어과";
-	kim.name = "김씨";
-	kim.studentId = 2211;
-	kim.telephone = "123-4567-8912";
-
-	kim.print();
-
-	
 
 	return 0;
 }
