140a24baefa51012ef7d8fda788ca7cb1f9da733
huihyeon06 <agnea0829@gmail.com>
2023-04-18 14:48:20 +0900

    멤버변수 동적할당

--- 2023 CPP 202/문법/class 중급.cpp
+++ 2023 CPP 202/문법/class 중급.cpp
@@ -1,21 +1,26 @@
 ﻿#include<iostream>
+#include<string.h>
 using namespace std;
 
 class MString {
 public:
-	MString(const char* str) //c++에서는 const char 와 char를 구분함
+	MString(const char* str) //c++에서는 const char 와 char를 구분함, +이거는 문자열 받은 것이기 때문에 const여야 함
 	{
-		c_str_ = str;
+		unsigned int l = strlen(str);
+		c_str_ = new char[l + 1];  // '\0'(널문자)가 들어갈 공간 +1
+		strcpy(c_str_, str);
+		size_ = l;
+		//c_str_ = str;
 	}
 	unsigned int size(void) {
 		return size_;
 	}
-	const char* c_str(void) {
+	char* c_str(void) {
 		return c_str_;
 	}
 private:
 	unsigned int size_; //문자열의 길이
-	const char* c_str_; //문자열의 시작주소
+	char* c_str_; //문자열의 시작주소
 };
 
 int main(void) {
