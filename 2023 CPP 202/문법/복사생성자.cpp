d9bae7f43463f0babb81098bea73a8765e8727d5
huihyeon06 <agnea0829@gmail.com>
2023-05-16 13:53:09 +0900

    일반적인 생성자, 소멸자

---
+++ 2023 CPP 202/문법/복사생성자.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<String.h>
+
+using namespace std;
+
+class Munja {
+
+public:
+	Munja(const char* jhqz) {
+		// 3("abc")+1('\0')
+		str_ = new char[strlen(jhqz)+1];
+		strcpy(str_, jhqz);
+		cout << "일반생성자 호출" << endl;
+	}
+
+	~Munja() {
+		delete[] str_;
+		cout << "소멸자 호출" << endl;
+	}
+
+private:
+	char* str_;
+};
+
+int main(void) {
+	Munja m1 = Munja("abc");
+	return 0;
+}
