b56d6d17fc00152aad5f906ba6e3930abe2e276e
huihyeon06 <agnea0829@gmail.com>
2023-05-23 15:12:17 +0900

    [SFML] 픽셀좌표를 격자(grid)좌표로 변환

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -5,9 +5,11 @@
 using namespace sf;
 
 int main(void) {
-	const int WIDTH = 1000;
-	const int HEIGHT = 800;
-	const int BLOCK_SIZE = 50;
+	const int WIDTH = 1000;						//픽셀 너비
+	const int HEIGHT = 800;						//픽셀 높이
+	const int BLOCK_SIZE = 50;					//한 캉이 가지고 있는 픽셀
+	const int G_WIDTH = WIDTH / BLOCK_SIZE;		//그리드의 너비
+	const int G_HEIGHT = HEIGHT / BLOCK_SIZE;	//그리드의 높이
 
 	srand(time(NULL));
 
@@ -15,17 +17,18 @@ int main(void) {
 
 	//컴퓨터가 1초 동안 처리하는 횟수를 60으로 제한한다
 	//Frame Per Second를 60으로 조절
-	window.setFramerateLimit(60);
+	window.setFramerateLimit(20);
 
 	RectangleShape snake;
+	int snake_x = 1, snake_y = 2;
 	snake.setFillColor(Color::White);
-	snake.setPosition(100, 300);
+	snake.setPosition(snake_x*BLOCK_SIZE, snake_y*BLOCK_SIZE);
 	snake.setSize(Vector2f(BLOCK_SIZE, BLOCK_SIZE));
 
 	RectangleShape apple;
+	int apple_x = rand() % G_WIDTH, apple_y = rand() % G_HEIGHT;
 	apple.setFillColor(Color::Red);
-	//640에 50을 뺀 이유는 사과가 화면 밖에 벗어나지 않게 하기 위함
-	apple.setPosition(rand()%(WIDTH - BLOCK_SIZE), rand()%(HEIGHT - BLOCK_SIZE));
+	apple.setPosition(apple_x * BLOCK_SIZE, apple_y * BLOCK_SIZE);
 	apple.setSize(Vector2f(BLOCK_SIZE, BLOCK_SIZE));
 
 	while (window.isOpen()) {
@@ -35,19 +38,28 @@ int main(void) {
 			if (e.type == Event::Closed)
 				window.close();
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Right))
-			snake.move(5, 0);
-		else if (Keyboard::isKeyPressed(Keyboard::Left))
-			snake.move(-5, 0);
-		else if (Keyboard::isKeyPressed(Keyboard::Up))
-			snake.move(0, -5);
-		else if (Keyboard::isKeyPressed(Keyboard::Down))
-			snake.move(0, 5);
+		if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			snake_x++;
+			snake.move(1 * BLOCK_SIZE, 0);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			snake_x--;
+			snake.move(-BLOCK_SIZE, 0);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			snake_y--;
+			snake.move(0, -BLOCK_SIZE);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			snake_y++;
+			snake.move(0, 1 * BLOCK_SIZE);
+		}
 		
 		//intersects -> 교집합(겹치는 부분)
 		//뱀이 사과를 먹었을 때, 사과의 위치를 랜덤으로 리스폰
 		if (snake.getGlobalBounds().intersects(apple.getGlobalBounds())) {
-			apple.setPosition(rand() % (WIDTH - BLOCK_SIZE), rand() % (HEIGHT - BLOCK_SIZE));
+			apple_x = rand() % G_WIDTH, apple_y = rand() % G_HEIGHT;
+			apple.setPosition(apple_x * BLOCK_SIZE, apple_y * BLOCK_SIZE);
 		}
 
 		window.clear();
