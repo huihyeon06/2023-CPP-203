82ad6b939a5e26849ece30aeee7edb32c01df3a8
huihyeon06 <agnea0829@gmail.com>
2023-05-26 08:57:23 +0900

    얕은복사 생성자

---
+++ 2023 CPP 202/문제풀이.cpp
@@ -0,0 +1,31 @@
+
+
+class IntArray {
+public:
+	IntArray(int* arr, int size) {
+		arr_ = new int[size];
+		size_ = size;
+		for (int i = 0; i < size_; i++) {
+			arr_[i] = arr[i];
+		}
+	}
+
+	//얕은복사
+	IntArray(const IntArray& rhs) 
+		:size_(rhs.size_), arr_(rhs.arr_)
+	{
+		size_ = rhs.size_;
+		arr_ = rhs.arr_;
+	}
+
+private:
+	int* arr_;
+	int size_; //arr의 크기
+};
+
+int main(void) {
+	int arr[] = { 1, 2, 3 };
+	IntArray ia = IntArray(arr, sizeof(arr)/sizeof(*arr));
+	IntArray ca = ia; //복사생성자 호출
+	return 0;
+}
