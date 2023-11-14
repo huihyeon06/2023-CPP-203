536b76517b2582b72ef4aae40b3b41bce10bc8b8
huihyeon06 <agnea0829@gmail.com>
2023-11-14 13:40:58 +0900

    static 멤버함수는 객체를 생성하지 않아도 호출할 수 있다

--- 2023 CPP 202/static.cpp
+++ 2023 CPP 202/static.cpp
@@ -6,6 +6,11 @@ using namespace std;
 class Champ {
 public:
 	Champ(const string& name);
+	static int get_cnt(void) {
+		return cnt_;
+	}
+	//객체에 있는 걸 활용하려면 객체를 생성해야하는 데 
+	//위와 같은 static을 사용하면 객체 생성을 하지 않아도 출력할 수 있음
 private:
 	static int cnt_;
 
@@ -25,5 +30,10 @@ int main(void) {
 	//Champ 형 객체의 갯수는 2개
 	//메모리에 cnt_ 변수는 한 개만 존재
 	//cnt_의 최종값은 2
+
+	//static 멤버함수는 객체를 생성하지 않아도 호출할 수 있다
+	cout << Champ::get_cnt() << endl;
+	cout << a->get_cnt() << endl;
+	cout << b->get_cnt() << endl;
 	return 0;
 }
